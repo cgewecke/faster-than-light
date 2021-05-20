cb3dae624d83f4b00e8c09f7d11d74607bf06c6b
David Kneel <david.kneel@protonmail.com>
2021-05-20 19:55:19 +0100

    :heavy_plus_sign: Add Solhint.

--- contracts/YieldFarmingToken.sol
+++ contracts/YieldFarmingToken.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity >=0.8.0 <0.9.0;
+
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
@@ -7,6 +8,7 @@ contract YieldFarmingToken is ERC20Burnable, Ownable {
     constructor (
         string memory name,
         string memory symbol
+    // solhint-disable-next-line no-empty-blocks
     ) ERC20(name, symbol) {
     }
     function mint(address recipient, uint amount) public onlyOwner{
